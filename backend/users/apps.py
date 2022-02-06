2dbe0016a4a283d27bcbd87b74b22d1c7cd37578
Pablo Olmedo Dorado <pablolmedorado@gmail.com>
2022-02-06 15:20:07 +0100

    fix(events): update calendar events that rely on user acronym when an user is updated

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -5,3 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 class UsersConfig(AppConfig):
     name = "users"
     verbose_name = _("usuarios")
+
+    def ready(self):
+        import users.signals  # noqa
---
+++ backend/users/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import User
+from events.models import EventType
+
+
+@receiver(post_save, sender=User)
+def update_calendar_event_names(sender, instance, created, raw, *args, **kwargs):
+    """
+    Updates support & holidays calendar event names in case user acronym had changed
+    """
+    if not created:
+        instance.events.filter(
+            type__system_slug__in=[EventType.SystemSlug.SUPPORT, EventType.SystemSlug.HOLIDAY]
+        ).update(name=instance.acronym)
